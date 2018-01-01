9a1e928c5b2db28bc2170e98731e5d71c76e2789
so2bin <bbhe_work@163.com>
2018-01-01 13:12:19 +0800

    完善登陆

--- userinfo/viewsets/validate.py
+++ userinfo/viewsets/validate.py
@@ -2,11 +2,13 @@ import logging
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.db import transaction
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
 
 from tools import exceptions as wter
 from tools.utils import validators, validatecode
+from userinfo.models import Accounts
 
 log = logging.getLogger('wed')
 
@@ -38,18 +40,23 @@ class RegisterValidateViewSet(APIView):
     """
     带上验证码完成注册
     """
+    @transaction.atomic
     def post(self, request):
         data = request.data
         phone = data.get('phone')
         code = data.get('code')
         password = data.get('password')
-        if not phone or not code or not password:
+        nickname = data.get('nickname')
+        if not phone or not code or not password or not nickname:
             return Response({'status': 1, 'msg': '数据不全'})
         valid, msg = validatecode.validate_code(phone, code)
         if not valid:
             return Response({'status': 1, 'msg': msg})
         user = User.objects.create_user(phone, password=password)
-        return Response({'status': 0, 'data': {}})
+        Accounts.objects.create(user_id=user.id, phone=user.username, nickname=nickname)
+        return Response({'status': 0, 'data': {
+            'id': user.id, 'phone': user.username, 'nickname': nickname
+        }})
 
 
 class LoginViewSet(APIView):
@@ -64,11 +71,14 @@ class LoginViewSet(APIView):
             return Response({'status': 1, 'msg': '帐号/密码不能为空'})
         user = authenticate(username=phone, password=password)
         if not user:
-            return Response({'status': 1, 'msg': '登陆验证失败'})
+            return Response({'status': 1, 'msg': '登陆验证失败，无效帐号密码'})
         else:
             if user.is_active:
                 login(request, user)
-                return Response({'status': 0})
+                uinfo = Accounts.objects.get(user_id=user.id)
+                return Response({'status': 0, 'data': {
+                    'id': user.id, 'phone': user.username, 'nickname': uinfo.nickname
+                }})
             else:
                 return Response({'status': 1, 'msg': '用户已失效'})
 
